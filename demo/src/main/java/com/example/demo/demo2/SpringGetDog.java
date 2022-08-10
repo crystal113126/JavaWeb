dbc12b155c69722d67b6ccec02ce8b6127cd349d
Jingia <jingx113126@gmail.com>
2022-08-09 23:18:28 -0400

    test first example with spring

---
+++ demo/src/main/java/com/example/demo/demo2/SpringGetDog.java
@@ -0,0 +1,16 @@
+package com.example.demo.demo2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringGetDog {
+    public static void main(String[] args) {
+        String resource = "spring.xml"; //define file;
+        ApplicationContext context = new ClassPathXmlApplicationContext(resource);
+        Dog dog = (Dog) context.getBean("dog");
+        Dog dog2 = context.getBean(Dog.class);
+        dog2.hello();
+
+
+    }
+}
